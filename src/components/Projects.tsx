853db3ba7499d0f38732758354405afc6f7e2c5a
manancodes <manancodes.dev@gmail.com>
2025-05-22 01:14:44 +0530

    Added more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,6 +29,20 @@ type Project = {
 const ProjectsData: Project[] = [
   {
     id: 1,
+    projectName: "Hud Hud Hub",
+    description:
+      "Hud Hud is a Qatar based e-commerce app for grocery delivery, features include cart, order management, favourites, product catalog, and secure checkout",
+    technologies: ["React-Native", "Expo", "Tailwind CSS"],
+    imageSrc: "/HudHudApp.png",
+    links: [
+      {
+        label: "App Store",
+        url: "https://apps.apple.com/sg/app/hudhudhub/id6499540516",
+      },
+    ],
+  },
+  {
+    id: 2,
     projectName: "Screenbows",
     description:
       "Screenbows is a web-based color generation tool that allows you to create and download beautiful color palettes as image files for your design and development projects.",
@@ -40,22 +54,18 @@ const ProjectsData: Project[] = [
     ],
   },
   {
-    id: 2,
-    projectName: "Ministry of Talent",
-    description:
-      "A proof-of-concept SaaS platform to empower job seekers with AI-driven, customized resumes via MOT-Profile, helping craft professional resumes to impress employers.",
-    technologies: ["React", "Next.js", "Tailwind CSS"],
-    imageSrc: "/MotDesktop.png",
+    id: 3,
+    projectName: "PixelCast",
+    description: "PixelCast is a simple weather app with Pixel Design.",
+    technologies: ["React-Native", "Expo"],
+    imageSrc: "/PixelCastDesktop.png",
     links: [
-      {
-        label: "Code",
-        url: "https://github.com/manancodes/Ministry-of-talent",
-      },
-      { label: "Demo", url: "https://ministry-of-talent.netlify.app/" },
+      { label: "Code", url: "https://github.com/manancodes/PixelCast" },
+      { label: "Demo", url: "https://pixel-cast.vercel.app/" },
     ],
   },
   {
-    id: 3,
+    id: 4,
     projectName: "Usernaminator",
     description:
       "Usernaminator searches major social media platforms to find the perfect username for you.",
@@ -67,7 +77,7 @@ const ProjectsData: Project[] = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     projectName: "Wellpaper",
     description:
       "Awesome outfits for your phone. A wallpaper app with Search, Sort, Filters, and Quick-Filters.",
@@ -81,6 +91,21 @@ const ProjectsData: Project[] = [
       //   label: "Play Store",
       //   url: "https://play.google.com/store/apps/wellpaper",
       // },
+    ],
+  },
+  {
+    id: 6,
+    projectName: "Ministry of Talent",
+    description:
+      "A proof-of-concept SaaS platform to empower job seekers with AI-driven, customized resumes via MOT-Profile, helping craft professional resumes to impress employers.",
+    technologies: ["React", "Next.js", "Tailwind CSS"],
+    imageSrc: "/MotDesktop.png",
+    links: [
+      {
+        label: "Code",
+        url: "https://github.com/manancodes/Ministry-of-talent",
+      },
+      { label: "Demo", url: "https://ministry-of-talent.netlify.app/" },
     ],
   },
 ];
@@ -123,9 +148,9 @@ const Projects = () => {
             return (
               <div
                 key={id}
-                className="mx-auto flex max-w-lg items-center justify-center rounded-lg bg-neutral-900 bg-opacity-30 border border-neutral-800"
+                className="mx-auto flex max-w-lg w-full items-center justify-center rounded-lg bg-neutral-900 bg-opacity-30 border border-neutral-800"
               >
-                <div className="relative p-4 h-full flex flex-col justify-between">
+                <div className="relative p-4 h-full w-full flex flex-col justify-between">
                   <div>
                     <Image
                       src={imageSrc}
@@ -137,37 +162,44 @@ const Projects = () => {
                     <h2 className="text-xl font-semibold text-gray-200">
                       {projectName}
                     </h2>
-                    <p className="text-gray-400">{description}</p>
+                    <p className="text-gray-400 w-full">{description}</p>
                   </div>
-                  <div className="flex flex-wrap mt-4">
-                    {technologies.map((tech: string, index: number) => (
-                      <div key={index} className="flex items-center mr-2 mb-2">
-                        {techIcons[tech] ? (
-                          <>
-                            {techIcons[tech]}
-                            <span className="ml-2 text-sm text-gray-400">
+                  <div>
+                    <div className="flex flex-wrap mt-4">
+                      {technologies.map((tech: string, index: number) => (
+                        <div
+                          key={index}
+                          className="flex items-center mr-2 mb-2"
+                        >
+                          {techIcons[tech] ? (
+                            <>
+                              {techIcons[tech]}
+                              <span className="ml-2 text-sm text-gray-400">
+                                {tech}
+                              </span>
+                            </>
+                          ) : (
+                            <span className="text-sm text-gray-400">
                               {tech}
                             </span>
-                          </>
-                        ) : (
-                          <span className="text-sm text-gray-400">{tech}</span>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="flex flex-wrap mt-4 gap-2">
-                    {links.map(({ label, url }, index) => (
-                      <Link
-                        key={index}
-                        href={url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex items-center px-3 py-1 text-sm text-gray-400 bg-neutral-800 border border-neutral-700 rounded-md hover:bg-neutral-700 transition-colors"
-                      >
-                        {linkIcons[label] || null}
-                        {label}
-                      </Link>
-                    ))}
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                    <div className="flex flex-wrap mt-4 gap-2">
+                      {links.map(({ label, url }, index) => (
+                        <Link
+                          key={index}
+                          href={url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="flex items-center px-3 py-1 text-sm text-gray-400 bg-neutral-800 border border-neutral-700 rounded-md hover:bg-neutral-700 transition-colors"
+                        >
+                          {linkIcons[label] || null}
+                          {label}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
